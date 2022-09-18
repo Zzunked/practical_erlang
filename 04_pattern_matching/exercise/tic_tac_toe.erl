5b61584ed2f8702a4ddf44b1a28b1a358503646a
Ivan Bogdanov <ivan.bogdanov@ringcentral.com>
2022-09-18 15:27:36 +0400

    Lesson 4 is done

--- 04_pattern_matching/exercise/tic_tac_toe.erl
+++ 04_pattern_matching/exercise/tic_tac_toe.erl
@@ -1,6 +1,9 @@
 -module(tic_tac_toe).
 
--export([new_game/0, win/1, move/3]).
+-export([new_game/0, 
+		win/1, 
+		move/3
+		]).
 
 
 new_game() ->
@@ -10,8 +13,162 @@ new_game() ->
 
 
 win(GameState) ->
-    no_win.
+    case GameState of
+        {{Winner, Winner, Winner},
+         {_, _, _},
+         {_, _, _}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{_, _, _},
+         {Winner, Winner, Winner},
+         {_, _, _}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{_, _, _},
+         {_, _, _},
+         {Winner, Winner, Winner}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{Winner, _, _},
+         {Winner, _, _},
+         {Winner, _, _}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{_, Winner, _},
+         {_, Winner, _},
+         {_, Winner, _}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{_, _, Winner},
+         {_, _, Winner},
+         {_, _, Winner}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{Winner, _, _},
+         {_, Winner, _},
+         {_, _, Winner}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{_, _, Winner},
+         {_, Winner, _},
+         {Winner, _, _}} when Winner =:= x; Winner =:= o -> {win, Winner};
+        {{_, _, _},
+         {_, _, _},
+         {_, _, _}} -> no_win
+	end.
 
 
 move(Cell, Player, GameState) ->
-    {error, invalid_move}.
+	if 
+		Cell =:= 1 ->
+			case GameState of
+				{{Input, Value2, Value3},
+				 {Value4, Value5, Value6},
+				 {Value7, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Player, Value2, Value3},
+                         {Value4, Value5, Value6},
+                         {Value7, Value8, Value9}}};
+				{{Input, _, _},
+				 {_, _, _},
+				 {_, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 2 ->
+			case GameState of
+				{{Value1, Input, Value3},
+				 {Value4, Value5, Value6},
+				 {Value7, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Player, Value3},
+                         {Value4, Value5, Value6},
+                         {Value7, Value8, Value9}}};
+				{{_, Input, _},
+				 {_, _, _},
+				 {_, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 3 ->
+			case GameState of
+				{{Value1, Value2, Input},
+				 {Value4, Value5, Value6},
+				 {Value7, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Player},
+                         {Value4, Value5, Value6},
+                         {Value7, Value8, Value9}}};
+				{{_, _, Input},
+				 {_, _, _},
+				 {_, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 4 ->
+			case GameState of
+				{{Value1, Value2, Value3},
+				 {Input, Value5, Value6},
+				 {Value7, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Value3},
+                         {Player, Value5, Value6},
+                         {Value7, Value8, Value9}}};
+				{{_, _, _},
+				 {Input, _, _},
+				 {_, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 5 ->
+			case GameState of
+				{{Value1, Value2, Value3},
+				 {Value4, Input, Value6},
+				 {Value7, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Value3},
+                         {Value4, Player, Value6},
+                         {Value7, Value8, Value9}}};
+				{{_, _, _},
+				 {_, Input, _},
+				 {_, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 6 ->
+			case GameState of
+				{{Value1, Value2, Value3},
+				 {Value4, Value5, Input},
+				 {Value7, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Value3},
+                         {Value4, Value5, Player},
+                         {Value7, Value8, Value9}}};
+				{{_, _, _},
+				 {_, _, Input},
+				 {_, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 7 ->
+			case GameState of
+				{{Value1, Value2, Value3},
+				 {Value4, Value5, Value6},
+				 {Input, Value8, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Value3},
+                         {Value4, Value5, Value6},
+                         {Player, Value8, Value9}}};
+				{{_, _, _},
+				 {_, _, _},
+				 {Input, _, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 8 ->
+			case GameState of
+				{{Value1, Value2, Value3},
+				 {Value4, Value5, Value6},
+				 {Value7, Input, Value9}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Value3},
+                         {Value4, Value5, Value6},
+                         {Value7, Player, Value9}}};
+				{{_, _, _},
+				 {_, _, _},
+				 {_, Input, _}}  when Input =/= f -> {error, invalid_move}
+			end;
+		Cell =:= 9 ->
+			case GameState of
+				{{Value1, Value2, Value3},
+				 {Value4, Value5, Value6},
+				 {Value7, Value8, Input}}  when Input =:= f -> 
+                    {ok, {{Value1, Value2, Value3},
+                         {Value4, Value5, Value6},
+                         {Value7, Value8, Player}}};
+				{{_, _, _},
+				 {_, _, _},
+				 {_, _, Input}}  when Input =/= f -> {error, invalid_move}
+			end;
+		true -> {error, invalid_move}
+    end.
+
+
+
+
+
+			%case GameState of
+				%{{Input, Value, Value},
+				 %{Value, Value, Value},
+				 %{Value, Value, Value}} -> 
+					%if 
+						%Input =:= f -> 
+							%{{Player, Value, Value},
+							 %{Value, Value, Value},
+							 %{Value, Value, Value}};
+						%true -> {error, invalid_move}
+					%end;
--- 04_pattern_matching/exercise_hard/fizz_buzz.erl
+++ 04_pattern_matching/exercise_hard/fizz_buzz.erl
@@ -1,2 +1,33 @@
+-module(fizz_buzz).
+-export([fizzbuzz/0]).
+
+
 %% Реализовать Fizz Buzz
 %% https://habr.com/ru/post/298134/
+
+%Напишите программу, которая выводит на экран числа от 1 до 100. 
+%При этом вместо чисел, кратных трем, программа должна выводить слово «Fizz», 
+%а вместо чисел, кратных пяти — слово «Buzz». Если число кратно и 3, и 5, 
+%то программа должна выводить слово «FizzBuzz»
+
+
+substitude(ListOfNumbers) -> substitude(ListOfNumbers, []).
+
+substitude([], Acc) -> lists:reverse(Acc);
+substitude([Number|T], Acc) ->
+    if 
+        Number rem 3 =:= 0, Number rem 5 =/= 0 -> substitude(T, ["Fizz" | Acc]);
+        Number rem 3 =/= 0, Number rem 5 =:= 0 -> substitude(T, ["Buzz" | Acc]);
+        Number rem 3 =:= 0, Number rem 5 =:= 0 -> substitude(T, ["FizzBuzz" | Acc]);
+        true -> substitude(T, [Number | Acc])
+    end.
+
+print_values_one_by_one([]) -> ok;
+print_values_one_by_one([H|T]) ->
+    io:format("~p, ", [H]),
+    print_values_one_by_one(T).
+
+fizzbuzz() ->
+    ListOfNumbers = lists:seq(1, 100),
+    FizzBuzzList = substitude(ListOfNumbers),
+    print_values_one_by_one(FizzBuzzList).
